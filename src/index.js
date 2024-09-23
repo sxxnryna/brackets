1894d44b1d2c75eb5cd3d00c43c8c25e1cd87d9f
Ryna <katrin_dr@naver.com>
2024-09-23 21:01:00 +0900

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let openBrackets = {};
+  let closeBrackets = {};
+  let sameBrackets = {};
+
+  for (let [open, close] of bracketsConfig) {
+    if (open === close) {
+      sameBrackets[open] = true;
+    } else {
+      openBrackets[open] = close;
+      closeBrackets[close] = open;
+    }
+  }
+
+  for (let char of str) {
+    if (sameBrackets[char]) {
+      if (stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (openBrackets[char]) {
+      stack.push(char);
+    } else if (closeBrackets[char]) {
+      if (stack.length === 0 || stack.pop() !== closeBrackets[char]) {
+        return false; // Mismatch found
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
